287167a6f28525825c5db8662530cad0769d8ce2
naegonggae <https://github.com/naegonggae/>
2022-12-23 14:46:59 +0900

    join 할때 userName이 중복되는지 체크하고 콘솔에 에러메세지 보내기 1. UserJoinRequest에 toEntity() 추가 2. UserRepository findByUserName() 추가 3. UserService에 userName 체크하고 에러메세지 보내기, 저장하기 구현

--- src/main/java/com/final_project_leesanghun_team2/service/UserService.java
+++ src/main/java/com/final_project_leesanghun_team2/service/UserService.java
@@ -1,5 +1,6 @@
 package com.final_project_leesanghun_team2.service;
 
+import com.final_project_leesanghun_team2.domain.User;
 import com.final_project_leesanghun_team2.domain.dto.UserDto;
 import com.final_project_leesanghun_team2.domain.dto.UserJoinRequest;
 import com.final_project_leesanghun_team2.repository.UserRepository;
@@ -13,6 +14,16 @@ public class UserService {
     private final UserRepository userRepository;
 
     public UserDto join(UserJoinRequest userJoinRequest) {
-        return new UserDto("", "");
+
+        userRepository.findByUserName(userJoinRequest.getUserName())
+                .ifPresent(user -> {throw new RuntimeException("해당 UserName이 중복됩니다.");
+                });
+
+        User savedUSer = userRepository.save(userJoinRequest.toEntity());
+        return UserDto.builder()
+                .id(savedUSer.getId())
+                .userName(savedUSer.getUserName())
+                .password(savedUSer.getPassword())
+                .build();
     }
 }
