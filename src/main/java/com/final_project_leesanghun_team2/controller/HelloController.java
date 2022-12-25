1b6121f735a7355d17e12a4ec3c568aa4ac24fcd
naegonggae <https://github.com/naegonggae/>
2022-12-25 10:39:43 +0900

    CI/CD test popin -> x-mas

--- src/main/java/com/final_project_leesanghun_team2/controller/HelloController.java
+++ src/main/java/com/final_project_leesanghun_team2/controller/HelloController.java
@@ -11,6 +11,6 @@ public class HelloController {
 
     @GetMapping("/hello")
     public ResponseEntity<String> hello() {
-        return ResponseEntity.ok().body("popin");
+        return ResponseEntity.ok().body("x-mas");
     }
 }
