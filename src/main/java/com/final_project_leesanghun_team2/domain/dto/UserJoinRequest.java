4bc63b34f1d4fd856aed4745e8dd3842156d1b58
naegonggae <https://github.com/naegonggae/>
2022-12-25 12:47:45 +0900

    비밀번호를 암호화해서 DB에 저장시키기 1. EncryptConfiguration, SecurityConfiguration 작성 2. Dependency - security 추가 3. UserJoinRequest.toEntity에 password 파라미터 추가 4. UserService save할때 toEntity 파라미터로 BCryptPasswordEncoder 메소드사용해서 암호화하기

--- src/main/java/com/final_project_leesanghun_team2/domain/dto/UserJoinRequest.java
+++ src/main/java/com/final_project_leesanghun_team2/domain/dto/UserJoinRequest.java
@@ -10,10 +10,10 @@ public class UserJoinRequest {
     private String userName;
     private String password;
 
-    public User toEntity() {
+    public User toEntity(String password) {
         return User.builder()
                 .userName(this.userName)
-                .password(this.password)
+                .password(password)
                 .build();
     }
 }
