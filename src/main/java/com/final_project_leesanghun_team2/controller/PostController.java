5f011ec1382f5c9befda3ef2a9e06928fce57eab
naegonggae <tkdtkd97@naver.com>
2023-01-20 01:33:59 +0900

    Post 수정, 삭제 기능 추가

--- src/main/java/com/final_project_leesanghun_team2/controller/PostController.java
+++ src/main/java/com/final_project_leesanghun_team2/controller/PostController.java
@@ -1,6 +1,8 @@
 package com.final_project_leesanghun_team2.controller;
 
 import com.final_project_leesanghun_team2.domain.entity.Post;
+import com.final_project_leesanghun_team2.exception.ErrorCode;
+import com.final_project_leesanghun_team2.exception.UserSnsException;
 import com.final_project_leesanghun_team2.repository.PostRepository;
 
 import lombok.RequiredArgsConstructor;
@@ -10,10 +12,7 @@ import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequestMapping("/posts")
@@ -54,6 +53,36 @@ public class PostController {
     public String submit(Post post) {
 
         postRepository.save(post);
+        return "redirect:/posts/list";
+    }
+
+    @GetMapping("/modify")
+    public String formModify(Model model, @RequestParam(required = false) Integer id) {
+        if (id == null) {
+            model.addAttribute("post", new Post());
+        } else {
+            Post post = postRepository.findById(id).orElseThrow(() -> new RuntimeException());
+            model.addAttribute("post", post);
+        }
+        return "/posts/modify";
+    }
+
+    @PostMapping("/modify")
+    public String submitModify(Post post) {
+
+        postRepository.save(post);
+        return "redirect:/posts/list";
+    }
+
+    @GetMapping("/delete")
+    public String delete(Model model, @RequestParam(required = false) Integer id) {
+        if (id == null) {
+            Post post = postRepository.findById(id)
+                    .orElseThrow(() -> new UserSnsException(ErrorCode.POST_NOT_FOUND));
+
+        } else {
+            postRepository.deleteById(id);
+        }
         return "redirect:/posts/list";
     }
 }
